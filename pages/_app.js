4fb355b3e33c37125635116561f138b00814c30a
kbu715 <kbu715@gmail.com>
2022-05-21 23:33:48 +0900

    feat: setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Community App with Next.js</title>
+
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
